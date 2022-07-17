346c03cd80a92cad9ee2ad5e4690cd70b6661123
JamesChesterman <chesterman261@sky.com>
2022-07-17 21:33:53 +0100

    Now an empty pane.

--- src/main/java/com/example/pomodoroguitest/HelloApplication.java
+++ src/main/java/com/example/pomodoroguitest/HelloApplication.java
@@ -3,18 +3,20 @@ package com.example.pomodoroguitest;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws IOException{
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 400, 400);
+        stage.setTitle("Hello");
         stage.setScene(scene);
         stage.show();
+        //So it's stage contains a scene which contains the panes.
     }
 
     public static void main(String[] args) {
